3981bd9f3440364b4f0a89ba1270102efa825aad
hogura88 <hogura88@gmail.com>
2024-05-10 17:46:12 +0900

    23章まで

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\HelloController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\RequestController;
+use App\Http\Controllers\ResponseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,10 +27,20 @@ Route::get('/hello', [HelloController::class, 'index']);
 
 Route::get('/products', [ProductController::class, 'index']);
 
+Route::get('/products/create', [ProductController::class, 'create']);
+
+Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+
 Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::get('/vendors/create', [VendorController::class, 'create']);
+
+Route::post('/vendors/store', [VendorController::class, 'store'])->name('vendors.store');
 
 Route::get('/vendors/{id}', [VendorController::class, 'show']);
 
 Route::get('/requests/create', [RequestController::class, 'create']);
 
 Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
+
+Route::get('/responses', [ResponseController::class, 'index']);
